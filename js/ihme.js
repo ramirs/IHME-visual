138b252a818720a4d19a8d12cdd7eb5311a10e3e
ramirs <ramirs2015@gmail.com>
2016-04-01 21:49:50 -0700

    minor updates, preparing ui control

--- js/ihme.js
+++ js/ihme.js
@@ -1,5 +1,13 @@
 (function(){
+
+	//full page splash - contact, credits, links to portfolio, etc.
 	$('.page_info').click(function(){
-		//do something? maybe?
+
 	});
+
+	//set up and maintain the context for slected regions
+	$(".country_code").on('change', function(){
+		// console.log($('#country_code')[0].value);
+	});
+
 })();
