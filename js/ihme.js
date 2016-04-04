44b34c870fb03196919f392a0e0b68b566f8d901
ramirs <ramirs2015@gmail.com>
2016-04-04 00:19:03 -0700

    second visual and text content fully functioning

--- js/ihme.js
+++ js/ihme.js
@@ -1,13 +1,26 @@
 (function(){
 
-	//full page splash - contact, credits, links to portfolio, etc.
-	$('.page_info').click(function(){
+	var content;
+	var selection = $('.country_code')[0];
 
+	var updateContent = function () {
+		var data = content[selection.value];
+		$('.content_header').text(data.name);
+		$('.content_main').text(data.content);
+		$('.content_link').html('<a href=\"' + data.link +'\" target=\"_blank\">More information about this region</a>');
+	};
+
+	$.getJSON("../data/content.json", function(data){
+		content = data;
+		updateContent();
 	});
 
-	//set up and maintain the context for slected regions
+	$('.page_info').click(function(){
+		//full page splash - contact, credits, links to portfolio, etc.
+	});
+
 	$(".country_code").on('change', function(){
-		// console.log($('#country_code')[0].value);
+		updateContent();
 	});
 
 })();
